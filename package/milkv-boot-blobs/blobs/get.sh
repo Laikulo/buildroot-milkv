1289ac537fa39125f00fb582c4f7222b63ed8d27
Aaron B. Haun <aaron@haun.guru>
2026-07-18 20:53:11 -0400

    feat: more kernel fun:

--- package/milkv-boot-blobs/blobs/get.sh
+++ package/milkv-boot-blobs/blobs/get.sh
@@ -4,4 +4,5 @@ set -xe
 [[ -f base.zip ]] || curl -Lo base.zip -\# 'https://github.com/milkv-duo/duo-buildroot-sdk/releases/download/v1.1.4/milkv-duo-sd-v1.1.4.img.zip'
 [[ -f milkv-duo-sd-v1.1.4.img ]] || unzip base.zip
 [[ -f 0.fat ]] || 7z x milkv-duo-sd-v1.1.4.img 0.fat
-[[ -f fip.bin ]] || 7z x 0.fat fip.bin boot.sd
+[[ -f fip.bin ]] || 7z x 0.fat fip.bin
+[[ -f boot.sd ]] || 7z x 0.fat boot.sd
